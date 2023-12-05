c772322674a515e69e87529ed4c0a98c1b1925c0
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-05 14:15:05 +0100

    cleaned

--- resources/views/components/card.blade.php
+++
@@ -1,6 +0,0 @@
-{{-- To overide/add classes to oour component we need to merge the attributes --}}
-{{-- $attributes represents all the attributes our component has been passed --}}
-<div {{$attributes->merge(['class' => 'bg-gray-50 border border-gray-200 rounded p-6'])}}>
-    {{$slot}}
-    {{-- The $slot variable is the placeholder for all content to be inserted --}}
-</div>
