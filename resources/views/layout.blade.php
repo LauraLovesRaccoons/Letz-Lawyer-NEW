b9ef098ecfe3efd2587c73f7c85a4d8e069faa30
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-06 15:44:14 +0100

    Update layout.blade.php
    fix x flash issue

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -73,7 +73,6 @@
         @yield('content')
     </main>
 
-    <x-flash-message />
 
     <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2023, All Rights reserved - LetzLawyer</p>
