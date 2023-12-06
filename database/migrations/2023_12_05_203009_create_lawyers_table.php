1da986230f16882f38dadce6262f336feea32efa
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-06 14:16:17 +0100

    Update 2023_12_05_203009_create_lawyers_table.php
    specialty

--- database/migrations/2023_12_05_203009_create_lawyers_table.php
+++ database/migrations/2023_12_05_203009_create_lawyers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('location');
-            $table->string('speciality');
+            $table->string('specialty');
             // Add other relevant fields
             $table->timestamps();
         });
