2ebd7ef7590a51a6ea3c2beef9dcdce86860980e
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-07 10:24:10 +0100

    Update view_lawyer_data.blade.php
    skeleton exists - adjustements needed later

--- resources/views/lawyers/view_lawyer_data.blade.php
+++ resources/views/lawyers/view_lawyer_data.blade.php
@@ -3,9 +3,7 @@
 @section('content')
     <x-card class="p-10 max-w-lg mx-auto mt-24">
         <header class="text-center">
-            <h2 class="text-2xl font-bold uppercase mb-1">
-                This is your Lawyer Profile
-            </h2>
+            <h2 class="text-2xl font-bold uppercase mb-1">This is your Lawyer Profile</h2>
         </header>
 
         <div class="mb-6">
@@ -29,10 +27,7 @@
         </div>
 
         <div class="mb-6">
-            <label for="languages" class="inline-block text-lg mb-2">
-                About me
-            </label>
-
+            <label for="languages" class="inline-block text-lg mb-2">Languages you speak</label>
             <textarea class="border border-gray-200 rounded p-2 w-full" rows="6" disabled>{{ $lawyer->languages }}</textarea>
 
         </div>
@@ -47,14 +42,32 @@
             <input type="text" value="{{ $lawyer->phone }}" class="border border-gray-200 rounded p-2 w-full" disabled />
         </div>
 
+        {{-- website - can be NULL in db --}}
         <div class="mb-6">
-            <label for="logo" class="inline-block text-lg mb-2">
-                Company Logo / Your Photo
-            </label>
+            <label for="url" class="inline-block text-lg mb-2">Website URL</label>
+            <input type='url' value='{{ $lawyer->url ?? "You have no website" }}'   {{-- ?? means if empty display that 'string' --}}
+                class="border border-gray-200 rounded p-2 w-full" disabled />
+        </div>
+
+        <div class="mb-6">
+            <label for="logo" class="inline-block text-lg mb-2">Company Logo / Your Photo</label>
             <img src="{{ asset('images/' . $lawyer->logo) }}" alt="{{ $lawyer->company }}" class="w-24 logo">
         </div>
 
         <div class="mb-6">
-            <label for="description" class="inline-block text-lg mb-2">
-                About me
-            </label>
+            <label for="description" class="inline-block text-lg mb-2">About me</label>
+            <textarea class="border border-gray-200 rounded p-2 w-full" rows="6" disabled>{{ $lawyer->description }}</textarea>
+        </div>
+
+        {{-- Edit and Delete buttons --}}
+        <div class="flex justify-between items-center">
+            <a href="/lawyers/{{ $lawyer->id }}/edit" class="button">Edit your lawyer profile</a>
+            <form method="POST" action="/lawyers/{{ $lawyer->id }}">
+                @csrf
+                @method('DELETE')
+                <button class="button"><strong>DELETE ACCOUNT - PERMANENTLY</strong></button>
+            </form>
+
+
+    </x-card>
+@endsection
