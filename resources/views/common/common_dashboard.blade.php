f76b1a20b3329afbff61dd72987472a1b733b457
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-12 15:13:32 +0100

    less cursed images

--- resources/views/common/common_dashboard.blade.php
+++ resources/views/common/common_dashboard.blade.php
@@ -16,7 +16,7 @@
         @foreach ($results as $user)
             <li>
                 <h3>{{ $user->name }}</h3>
-                <p><img style="width: 157px; box-sizing: border-box; padding: 10px 0;" src="https://thispersondoesnotexist.com/" alt="{{ $user->name }}'s picture is not loading properly."></p>
+                <p><img style="width: 157px; box-sizing: border-box; padding: 10px 0;" src="{{ asset('images/Logo_V0.3.png') }}" alt="{{ $user->name }}'s picture is not loading properly."></p>
                 <p>{{ $user->email }}</p>
                 {{-- <p>{{ $post->description }}</p>
                 <p>{{ $post->email }}</p>
