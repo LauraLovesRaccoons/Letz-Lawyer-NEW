96bdb0e0bf848bfaea0d66ede1e7ef1b64fe1386
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-13 13:39:11 +0100

    REAL profiles

--- resources/views/common/common_dashboard.blade.php
+++ resources/views/common/common_dashboard.blade.php
@@ -3,28 +3,44 @@
 @section('content')
     <style>
         body {
-            background-color: #e4dfd9; /* Light gold background color */
+            background-color: #e4dfd9;
+            /* Light gold background color */
         }
 
         .container {
-            background-color: #fff; /* White background color for content */
-            border-radius: 10px; /* Add some rounded corners */
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow */
+            background-color: #fff;
+            /* White background color for content */
+            border-radius: 10px;
+            /* Add some rounded corners */
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            /* Add a subtle box shadow */
             padding: 20px;
             margin-top: 20px;
             margin-bottom: 20px;
         }
 
-        h1, h2, h3, h4, h5, p {
-            color: #4b4a4a; /* Dark gray text color */
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        p {
+            color: #4b4a4a;
+            /* Dark gray text color */
         }
-        .container, h1{
-            background-color: #8391b1; 
+
+        .container,
+        h1 {
+            background-color: #8391b1;
         }
+
         .btn-primary {
-            background-color: #3e4f73; /* Light gold color for buttons */
-            border-color: #3e4f73; /* Border color for buttons */
-            color: #ffffff; /* Dark gray text color for buttons */
+            background-color: #3e4f73;
+            /* Light gold color for buttons */
+            border-color: #3e4f73;
+            /* Border color for buttons */
+            color: #ffffff;
+            /* Dark gray text color for buttons */
         }
     </style>
 
@@ -48,17 +64,19 @@
                         @foreach ($results as $user)
                             <li class="list-group-item" style="background-color: rgb(231, 204, 204) !important;">
                                 <h3 class="text-2xl font-bold mb-4">{{ $user->name }}</h3>
-                                <p class="mb-2"><img style="max-width: 251px;" class="img-fluid" src="{{ asset('images/Logo_V0.3.png') }}" alt="{{ $user->name }}'s picture"></p>
+                                <p class="mb-2"><img style="max-width: 251px;" class="img-fluid"
+                                        src="{{ asset('images/Logo_V0.3.png') }}" alt="{{ $user->name }}'s picture"></p>
                                 <p style="margin: 5px 0;">Email: {{ $user->email }}</p>
                                 <a href="/appointments/create" class="btn btn-primary btn-sm">View Profile</a>
                                 <br>
                             </li>
                         @endforeach
                         <br>
-                        <li class="list-group-item text-muted" style="background-color: #e6bcbc !important;">End of Results</li>
+                        <li class="list-group-item text-muted" style="background-color: #e6bcbc !important;">End of Results
+                        </li>
                     </ul>
                 @else
-                    <div class="row">
+                    {{-- <div class="row">
                         @foreach ($allLawyerPosts->take(4) as $post)
                             <div class="col-sm-6 mb-4">
                                 <div class="card">
@@ -71,7 +89,50 @@
                                 </div>
                             </div>
                         @endforeach
-                    </div>
+                    </div> --}}
+                    {{-- fake profiles --}}
+                    <ul class="list-group" style="background-color: #c9a4a4 !important;">
+                        {{-- 1 --}}
+                        <li class="list-group-item" style="background-color: rgb(231, 204, 204) !important;">
+                            <h3 class="text-2xl font-bold mb-4">Henry Masterlawyer</h3>
+                            <p class="mb-2"><img style="max-width: 251px;" class="img-fluid"
+                                    src="{{ asset('images/Logo_V0.3.png') }}" alt="XYZ's picture"></p>
+                            <p style="margin: 5px 0;">Specialty: Family Law</p>
+                            <p style="margin: 5px 0;">Email: henry@email.com</p>
+                            <p style="margin: 5px 0;">Phone: +352 123 456</p>
+                            <p style="margin: 5px 0;">Henry is a passionate and experienced lawyer with a
+                                strong commitment to justice and client satisfaction. She has a proven track record of
+                                success in a variety of legal matters, including family law, personal injury, and criminal
+                                defense. Anya is known for her compassionate approach to her clients, her strategic
+                                thinking, and her ability to achieve positive outcomes in even the most challenging cases.
+                            </p>
+                            <br>
+                        </li>
+                        {{-- 1 end --}}
+                        {{-- 2 --}}
+                        <li class="list-group-item" style="background-color: rgb(231, 204, 204) !important;">
+                            <h3 class="text-2xl font-bold mb-4">Anya Petrova</h3>
+                            <p class="mb-2"><img style="max-width: 251px;" class="img-fluid"
+                                    src="{{ asset('images/Logo_V0.3.png') }}" alt="XYZ's picture"></p>
+                            <p style="margin: 5px 0;">Specialty: Divorce Law</p>
+                            <p style="margin: 5px 0;">Email: anya.petrova@email.com</p>
+                            <p style="margin: 5px 0;">Phone: +352 123 456 789</p>
+                            <p style="margin: 5px 0;">Anya Petrova is a compassionate and experienced divorce lawyer with a
+                                strong commitment to navigating the complexities of divorce with empathy and understanding.
+                                She has a proven track record of success in helping clients achieve their desired outcomes
+                                in divorce proceedings, including child custody arrangements, spousal support
+                                determinations, and property division agreements. Anya is dedicated to guiding her clients
+                                through this challenging time with professionalism, discretion, and a genuine concern for
+                                their well-being.
+                            </p>
+                            <br>
+                        </li>
+                        {{-- 2 end --}}
+                        <br>
+                        <li class="list-group-item text-muted" style="background-color: #e6bcbc !important;">End of Results
+                        </li>
+                    </ul>
+                    {{-- end fake --}}
                     <p class="text-muted mt-4">End of Posts</p>
                 @endif
             </div>
@@ -79,9 +140,11 @@
             <div class="col-md-4">
                 @if (auth()->check())
                     @if (auth()->user()->is_lawyer == 1)
-                        <a href="/lawyers/lawyer_dashboard" class="btn btn-primary btn-lg btn-block mt-4">Go to Lawyer Dashboard</a>
+                        <a href="/lawyers/lawyer_dashboard" class="btn btn-primary btn-lg btn-block mt-4">Go to Lawyer
+                            Dashboard</a>
                     @else
-                        <a href="/client/client_dashboard" class="btn btn-primary btn-lg btn-block mt-4">Go to Client Dashboard</a>
+                        <a href="/client/client_dashboard" class="btn btn-primary btn-lg btn-block mt-4">Go to Client
+                            Dashboard</a>
                     @endif
                 @endif
             </div>
--- resources/views/lawyers/lawyer_dashboard.blade.php
+++ resources/views/lawyers/lawyer_dashboard.blade.php
@@ -103,6 +103,45 @@
                 <label for="specialty">Select Specialties</label>
                 <select name="specialty" size="6">
                     <!-- Options remain the same -->
+                    <option value="0" data-id="0" selected disabled hidden>Select 1 specialty</option>
+                    <option value="2" data-id="2">Family Law</option>
+                    <option value="3" data-id="3">Criminal Law</option>
+                    <option value="4" data-id="4">Civil Law</option>
+                    <option value="5" data-id="5">Intellectual Property Law</option>
+                    <option value="6" data-id="6">Real Estate Law</option>
+                    <option value="7" data-id="7">Tax Law</option>
+                    <option value="8" data-id="8">Immigration Law</option>
+                    <option value="9" data-id="9">Labor and Employment Law</option>
+                    <option value="10" data-id="10">Health Law</option>
+                    <option value="11" data-id="11">Environmental Law</option>
+                    <option value="12" data-id="12">Bankruptcy Law</option>
+                    <option value="13" data-id="13">Personal Injury Law</option>
+                    <option value="14" data-id="14">Estate Planning Law</option>
+                    <option value="15" data-id="15">Business Law</option>
+                    <option value="16" data-id="16">Entertainment Law</option>
+                    <option value="17" data-id="17">Sports Law</option>
+                    <option value="18" data-id="18">Fashion Law</option>
+                    <option value="19" data-id="19">Art Law</option>
+                    <option value="20" data-id="20">Education Law</option>
+                    <option value="21" data-id="21">Government Law</option>
+                    <option value="22" data-id="22">International Law</option>
+                    <option value="23" data-id="23">Non-Profit Law</option>
+                    <option value="24" data-id="24">Intellectual Property</option>
+                    <option value="25" data-id="25">Technology Law</option>
+                    <option value="26" data-id="26">Media Law</option>
+                    <option value="27" data-id="27">Telecommunications Law</option>
+                    <option value="28" data-id="28">Energy Law</option>
+                    <option value="29" data-id="29">Construction Law</option>
+                    <option value="30" data-id="30">Transportation Law</option>
+                    <option value="31" data-id="31">Agriculture Law</option>
+                    <option value="32" data-id="32">Corporate Lawyer</option>
+                    <option value="33" data-id="33">Mergers and Acquisitions Lawyer</option>
+                    <option value="34" data-id="34">Finance Lawyer</option>
+                    <option value="35" data-id="35">Securities Lawyer</option>
+                    <option value="36" data-id="36">Banking Lawyer</option>
+                    {{-- 1 is other if someone messes with stuff as well as for stuff that isn't listed here --}}
+                    <option value="1" data-id="1">Other</option>
+                    {{-- not selected is an empty entry will be handled below --}}
                 </select>
                 @error('specialty')
                     <p class="text-danger">{{ $message }}</p>
@@ -121,6 +160,6 @@
             </div>
         </form>
 
-        <a href="/lawyers/lawyer_dashboard">Create New Post</a>
+
     </div>
 @endsection
