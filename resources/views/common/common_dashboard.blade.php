c8ba1ebb4fe290b1c7020dba71f7e50f3c6f1d6e
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2023-12-13 11:27:08 +0100

    Update common_dashboard.blade.php

--- resources/views/common/common_dashboard.blade.php
+++ resources/views/common/common_dashboard.blade.php
@@ -46,10 +46,12 @@
                             <li class="list-group-item">
                                 <h3 class="text-2xl font-bold mb-4">{{ $user->name }}</h3>
                                 <p class="mb-2"><img style="max-width: 251px;" class="img-fluid" src="{{ asset('images/Logo_V0.3.png') }}" alt="{{ $user->name }}'s picture"></p>
-                                <p>Email: {{ $user->email }}</p>
+                                <p style="margin: 5px 0;">Email: {{ $user->email }}</p>
                                 <a href="/appointments/create" class="btn btn-primary btn-sm">View Profile</a>
+                                <br>
                             </li>
                         @endforeach
+                        <br>
                         <li class="list-group-item text-muted">End of Results</li>
                     </ul>
                 @else
